27d68e98f38dcf4b8d27800b7d8d6268b889f765
hasanuz2001 <53890494+hasanuz2001@users.noreply.github.com>
2026-01-14 14:46:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const CONFIG = {
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: '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',
     GIST_ID: 'd88f1ebc50c5d37c857ee5961d6dba5c',
     API_BASE: null // Kerak emas
 };
