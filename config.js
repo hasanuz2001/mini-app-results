dac55099d072495b6c9ad43d09cd3521f82e3630
hasanuz2001 <53890494+hasanuz2001@users.noreply.github.com>
2026-01-14 14:24:10 +0500

    Change GITHUB_TOKEN in config.js
    Updated GITHUB_TOKEN to a new value.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const CONFIG = {
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: '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',
     GIST_ID: 'd88f1ebc50c5d37c857ee5961d6dba5c',
     API_BASE: null // Kerak emas
 };
